7b5a682a4ee07653f7ea124af25bb0491f5eb749
PitbullRufRuf <113531935+PitbullRufRuf@users.noreply.github.com>
2023-05-03 14:26:38 -0300

    Add files via upload

---
+++ flustats.js
@@ -0,0 +1,21 @@
+username = "tcperalta";
+password = "ttperolts";
+
+form = document.getElementById("login-form");
+form.addEventListener("submit",(event) =>
+{
+	writtenUsername = document.getElementById("username-input").value;
+	writtenPassword = document.getElementById("password-input").value;
+	if(writtenUsername == username && writtenPassword == password)
+	{
+		event.preventDefault();
+		window.location.href = "./loggedin.html"
+	}
+	else
+	{
+		event.preventDefault();
+		alertCredentials = document.getElementById("wrong-credentials");
+		alertCredentials.style.display = "block";
+		setInterval(() => {alertCredentials.style.display = "none";}, 5000);
+	}
+});
